309e673585f09b981dbe78ad7f1b72fd21e0e6e9
Matt Gacy <matt@mgacy.com>
2025-11-21 14:04:21 -0700

    Silence warning

--- Plugins/VersionFile/Extensions/String+Utils.swift
+++ Plugins/VersionFile/Extensions/String+Utils.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 /// Easily throw generic errors with a text description.
-extension String: LocalizedError {
+extension String: @retroactive Error {}
+extension String: @retroactive LocalizedError {
     public var errorDescription: String? {
         return self
     }
